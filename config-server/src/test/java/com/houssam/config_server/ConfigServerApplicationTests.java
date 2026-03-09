f21388ceff4087b232dc051cc043c4348b0da27d
houssam <lambarahoussam@gmail.com>
2026-03-09 12:56:32 +0000

    add structure et microservice

---
+++ config-server/src/test/java/com/houssam/config_server/ConfigServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.houssam.config_server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConfigServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
